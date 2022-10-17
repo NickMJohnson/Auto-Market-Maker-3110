e7b2057787fecc3b613cfc73e2e9d856830a9400
NickMJohnson <nickj2990@gmail.com>
2022-10-16 22:50:58 -0400

    Implemented json importing
    -from_json and initial abstract types t and user
    
    -implemented new_database. want to improve, maybe use from_json?

--- src/database.ml
+++ src/database.ml
@@ -1,2 +1,25 @@
 open Database
 open Yojson.Basic.Util
+
+type user = {
+  name : string;
+  usd : int;
+  brb : int;
+}
+
+let user_of_json j =
+  {
+    name = j |> member "name" |> to_string;
+    usd = j |> member "usd" |> to_int;
+    brb = j |> member "brb" |> to_int;
+  }
+
+type t = { users : user list }
+
+let from_json (str : string) : t =
+  let j = Yojson.Basic.from_string str in
+  { users = j |> member "users" |> to_list |> List.map user_of_json }
+
+let new_database : t = { users = [] }
+(*TODO: Make this better. Like it could use from_json on an empty db file we
+  keep updated*)
