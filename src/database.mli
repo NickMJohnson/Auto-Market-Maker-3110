53da4edc58f72b879b792d42cb25c20bd4d5ea22
NickMJohnson <nickj2990@gmail.com>
2022-10-16 21:48:38 -0400

    Completed specs for functions in database.mli
    -Still need to make specs more precise, but the idea is there

--- src/database.mli
+++ src/database.mli
@@ -1,6 +1,9 @@
 type t
+(**the abstract type of an object representing a bid/ask database with users and
+   orders. Users have usernames and a balance in cents of BRB and USD*)
 
 val new_database : t
+(**[new_database] is a database of type t with no users or orders*)
 
 val from_json : string -> t
 (**takes in a json string, creates abstract db type t*)
@@ -9,8 +12,15 @@ val new_user : t -> string -> t
 (**[new_user db name] is the database b with a new user with username name added*)
 
 val users : t -> string list
+(**[users db] is a list of the usernames of all the users in db*)
+
 val deposit : t -> string -> string -> int -> t
+(**[deposit db name curr amt] is db with user name having amt more of currency
+   curr Requires: name is valid name of a user in db, curr is either USD or BRB*)
+
 val withdraw : t -> string -> string -> int -> t
+(**[withdraw db name curr amt] is db with user name having amt less of currency
+   curr Requires: name is valid name of a user in db, curr is either USD or BRB*)
 
 val user_balance : t -> string -> string -> int
 (** Example: [user_balance db tony brb] is tony's balance of brbs (in brb cents) *)
