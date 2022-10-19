a541a174a7eec17d1aa78740a36cb325b2c2899e
NickMJohnson <nickj2990@gmail.com>
2022-10-18 20:58:19 -0400

    Implement database.ml - Add Makefile

--- src/database.ml
+++ src/database.ml
@@ -1,4 +1,3 @@
-open Database
 open Yojson.Basic.Util
 
 type user = {
@@ -29,3 +28,45 @@ let new_database : t = { users = [] }
 (*let to_json db = (`Assoc [("users", `Assoc [] 12)])*)
 (*TODO: use Yojson.to_string to create the json. Need to figure out how to go
   from our type t to Yojson type t *)
+
+(*[new_user db name] is the database b with a new user with username name
+  added *)
+let new_user db nm = { users = db.users @ [ { name = nm; usd = 0; brb = 0 } ] }
+
+(*[users db] is a list of the usernames of all the users in db*)
+let users db = List.map (fun x -> x.name) db.users
+
+(*[deposit db name curr amt] is db with user name having amt more of currency
+  curr Requires: name is valid name of a user in db, curr is either USD or BRB*)
+let deposit db name curr amt =
+  let cap = String.uppercase_ascii curr in
+  let rec find_user users prev =
+    match users with
+    | [] -> db.users
+    | h :: s ->
+        if h.name = name then
+          if cap = "USD" then prev @ [ { h with usd = h.usd + amt } ] @ s
+          else prev @ [ { h with brb = h.brb + amt } ] @ s
+        else find_user s (prev @ [ h ])
+  in
+  { users = find_user db.users [] }
+
+(*[withdraw db name curr amt] is db with user name having amt less of currency
+  curr Requires: name is valid name of a user in db, curr is either usd or brb*)
+let withdraw db name curr amt = deposit db name curr (-amt)
+
+(* Example: [user_balance db name curr] is tony's balance of curr (in brb
+   cents) *)
+let user_balance db name curr =
+  let cap = String.uppercase_ascii curr in
+  let rec find_user users =
+    match users with
+    | [] -> 0
+    | h :: s ->
+        if h.name = name then if cap = "USD" then h.usd else h.brb
+        else find_user s
+  in
+  find_user db.users
+
+(**[save_to_json db] is a json in string form that represents db.t*)
+(* let save_to_json db = *)
