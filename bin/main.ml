7a1322bef1f548898ca0580dab90943c9569cc04
NickMJohnson <nickj2990@gmail.com>
2022-10-21 00:08:33 -0400

    Implemented most of Command.ml
    -Implement most of the interface

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,64 @@
-
 open Database
+type object_phrase = string list
+let data_dir_prefix = "data" ^ Filename.dir_sep
+
+type command =
+  | NewDatabase of object_phrase
+  | Database of object_phrase
+  | Login of object_phrase
+  | New_User of object_phrase
+  | Home
+  | Account
+  | Admin
+  | Deposit of object_phrase
+  | Withdraw of object_phrase 
+  | View_Bal
+  | View_Users 
+  | Quit
+
+
+exception Empty
+exception Malformed
+
+
+let com z = 
+  match String.lowercase_ascii z with 
+  | "" -> raise Empty
+  | "quit" -> Quit
+  | "home" -> Home
+  | "account" -> Account
+  | "admin" -> Admin
+  | "view_balance" -> View_Bal
+  | "view_users" -> View_Users
+  | _ -> raise Malformed
+
+let coms z sl= 
+  let len = List.length sl in
+  match String.lowercase_ascii z with 
+  | "deposit" -> Deposit sl
+  | "witdraw" -> Withdraw sl
+  | _ when len > 1 -> raise Malformed
+  | "" -> raise Empty
+  | "new_database" -> NewDatabase sl
+  | "database" -> Database sl
+  | "login" -> Login sl
+  | "new_user" -> New_User sl
+  | _ -> raise Malformed
+
+let string_split s = List.filter (fun x -> x <> "") (String.split_on_char ' ' s)
+let parser s = 
+  match s with
+  |[] -> raise (Empty)
+  | h::[] -> com h
+  | h::t -> coms h t 
+
+let parse s = parser (string_split s)
+
+let rec print_users stl = 
+  match stl with 
+  | [] -> "no users"
+  | h::t -> h ^ " , " ^ print_users t
+
 let rec home (db : Database.t) (u : string)= 
   print_string
   "\n\n 
@@ -13,104 +72,124 @@ let rec home (db : Database.t) (u : string)=
   else ANSITerminal.(print_string [blue; Bold] ("\nInvalid input. \n"));
 
 
+and view_users db u =  
+  print_string(print_users (users db));
+  admin db u
 
 and admin db u =
   print_string
   "\n\n 
-    Pleae select a menu \n
+    Pleae type your preference to select a menu \n
     - View Users\n
     - Home\n";
+  print_string "> ";
   match read_line () with
   |exception End_of_file -> ()
-  |inp -> if "view users" = (String.lowercase_ascii inp) then home db u
+  |inp -> if "view users" = (String.lowercase_ascii inp) then view_users db u
   else if "home" = (String.lowercase_ascii inp) then home db u
   else ANSITerminal.(print_string [blue; Bold] ("\nInvalid input. \n"));
 
 
-and account (db : Database.t) (u : string) = 
-  print_endline "Current User is:\n";
-  print_string u;
-  print_string
-  "\n\n 
-    Pleae select a menu \n
-    - View Balance\n
-    - Deposit\n
-    - Withdraw\n
-    - Home\n
-    - Quit";
+
+
+and quit db  = home db (db_name db)
+
+and withdraw__ db u curr = 
+  print_endline "How much would you like to withdraw:\n";
+  print_endline "Type an amount \n";
+  print_string "> ";
   match read_line () with
   |exception End_of_file -> ()
-  |inp -> if "view balance" = (String.lowercase_ascii inp) then view_bal db u
-  else if "deposit" = (String.lowercase_ascii inp) then deposit_ db u
-  else if "withdraw" = (String.lowercase_ascii inp) then withdraw db u
-  else if "home" = (String.lowercase_ascii inp) then home db u
-  else if "quit" = (String.lowercase_ascii inp) then quit db u
+  |inp -> account (withdraw db u curr (int_of_string inp)) u 
+
+and withdraw_ db u = 
+  print_endline "What would you like to withdrawl?:\n";
+  print_endline "Type brb to withdraw brb or usd to withdraw usd \n";
+  print_string "> ";
+  match read_line () with
+  |exception End_of_file -> ()
+  |inp -> if "brb" = (String.lowercase_ascii inp) then withdraw__ db u (String.lowercase_ascii inp)
+  else if "usd" = (String.lowercase_ascii inp) then withdraw__ db u (String.lowercase_ascii inp)
   else ANSITerminal.(print_string [blue; Bold] ("\nInvalid input. \n"));
 
-and user_l f = 
-  print_endline "What is your name\n";
-  print_string "> ";
-  match read_line () with
-  |exception End_of_file -> ()
-  |user -> account f user
-
-and quit db u = home db u
-and withdraw db u = home db u
-
-and new_user1 f = 
-  print_endline "What is your name\n";
-  print_string "> ";
-  match read_line () with
-  |exception End_of_file -> ()
-  |user -> account (new_user f user) user
-
-
-and view_bal db u = 
-  print_endline "Your BRB balance is:\n";
-  print_int(user_balance db u "usd");
-  print_endline "Your BRB balance is:\n";
-  print_int(user_balance db u "brb");
-  account db u
 
 and deposit__ db u curr = 
   print_endline "How much would you like to deposit:\n";
+  print_endline "Type an amount \n";
+  print_string "> ";
   match read_line () with
   |exception End_of_file -> ()
   |inp -> account (deposit db u curr (int_of_string inp)) u 
 
 and deposit_ db u = 
   print_endline "What would you like to deposit?:\n";
+  print_endline "Type brb to deposit brb or usd to deposit usd \n";
+  print_string "> ";
   match read_line () with
   |exception End_of_file -> ()
   |inp -> if "brb" = (String.lowercase_ascii inp) then deposit__ db u (String.lowercase_ascii inp)
   else if "usd" = (String.lowercase_ascii inp) then deposit__ db u (String.lowercase_ascii inp)
   else ANSITerminal.(print_string [blue; Bold] ("\nInvalid input. \n"));
 
-
-
-and login f = 
-  print_endline "Would you like to login or create a new user\n";
+and account (db : Database.t) (u : string) = 
+  print_endline "Current User is:\n";
+  print_endline(u);
+  print_endline "Your BRB balance is:\n\n";
+  print_int(user_balance db u "usd");
+  print_endline " \n";
+  print_endline "Your BRB balance is:\n\n";
+  print_int(user_balance db u "brb");
+  print_string
+  "\n\n 
+    Pleae select a menu \n
+    - Deposit Currency\n
+    - Withdraw Currency\n
+    - Home\n
+    - Quit\n";
   print_string "> ";
   match read_line () with
   |exception End_of_file -> ()
-  |log -> if "login" = (String.lowercase_ascii log) then user_l f
-  else if "new user" = (String.lowercase_ascii log) then new_user1 f
-  else ANSITerminal.(print_string [blue; Bold] ("\nInvalid input. \n"));
+  |t -> begin match (parse t) with 
+  | exception Malformed -> account db u
+  | exception Empty -> account db u
+  | Deposit x -> deposit_ db u
+  | Withdraw x -> withdraw_ db u 
+  | Home -> home db u
+  | Quit -> quit db u
+  | _ -> account db u
+end
 
+and login f = 
+  print_endline "Would you like to login or create a new user\n";
+  print_endline "Type login [username] to login or new_user [username] \n";
+  print_string "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | t -> begin match (parse t) with 
+  | exception Malformed -> main ()
+  | exception Empty -> main ()
+  | Login x -> account f (List.hd x)
+  | New_User x -> account (new_user f (List.hd x)) (List.hd x)
+  | _ -> main ()
+end
 
 
 (** [main ()] prompts for the game to play, then starts it. *)
 and main () = 
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\n\nWelcome to the trading platform.\n";
-  print_endline "Please enter [load database [database name]] to load a database \n
-  or [new database] to create a new one";
-  print_string "> ";
+  print_endline "Please enter [database [database name]] to load a database \n
+  or [new_database database] to create a new one";
+  print_string "> \n";
   match read_line () with
   | exception End_of_file -> ()
-  | str -> if "new database" = (String.lowercase_ascii str) then (login (new_database "new")) else failwith "unimplemented" 
-  
-
+  | t -> begin match (parse t) with 
+  | exception Malformed -> main ()
+  | exception Empty -> main ()
+  | NewDatabase x -> login (new_database (List.hd x))
+  | Database x -> (Yojson.Basic.from_file (data_dir_prefix ^ (List.hd x) ^ ".json") |> Yojson.Basic.to_string |> from_json |> login)
+  | _ -> main ()
+end
 
 (* Execute the (from_json (Yojson.Basic.from_file (data_dir_prefix ^ str ^ ".json"))) engine. *)
 
