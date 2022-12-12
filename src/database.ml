4e1938ac89d0b8e3e2e6e457889282d15dd5f61c
NickMJohnson <nickj2990@gmail.com>
2022-12-12 13:41:02 -0500

    Sell orders maybe done -could be buggy

--- src/database.ml
+++ src/database.ml
@@ -101,8 +101,6 @@ let sort_orders db =
   }
 
 let rec buy_order name amt (rate : float) db =
-  (* let charge_orderer = withdraw name "usd" (amt *> rate) db in
-     charge_orderer *)
   db |> buy_order_filler { user = name; amount = amt; rate } |> sort_orders
 
 and buy_order_filler order db =
@@ -152,17 +150,57 @@ and buy_order_filler order db =
           buy_order_filler updated_order smaller_head
 
 let rec sell_order name amt (rate : float) db =
-  let charge_orderer = withdraw name "brb" amt db in
-  {
-    charge_orderer with
-    orders =
-      {
-        db.orders with
-        sell_orders =
-          { user = name; amount = amt; rate } :: db.orders.sell_orders;
-      };
-  }
-  |> sort_orders
+  db |> sell_order_filler { user = name; amount = amt; rate } |> sort_orders
+
+and sell_order_filler order db =
+  if order.amount <= 0 then db
+  else
+    match db.orders.buy_orders with
+    | [] ->
+        {
+          db with
+          orders =
+            { db.orders with sell_orders = order :: db.orders.sell_orders };
+        }
+    | h :: t when order.rate > h.rate ->
+        {
+          db with
+          orders =
+            { db.orders with sell_orders = order :: db.orders.sell_orders };
+        }
+    | h :: t ->
+        if h.amount <= order.amount then
+          let remove_head =
+            { db with orders = { db.orders with buy_orders = t } }
+            |> deposit h.user "brb" h.amount
+            |> withdraw order.user "brb" h.amount
+            |> deposit order.user "usd" (h.amount *> h.rate)
+            |> withdraw h.user "usd" (h.amount *> h.rate)
+          in
+          let updated_order = { order with amount = order.amount - h.amount } in
+          sell_order_filler updated_order remove_head
+        else
+          (*If the smallest sell order is enough to fulfill our order*)
+          let smaller_head =
+            {
+              db with
+              orders =
+                {
+                  db.orders with
+                  buy_orders = { h with amount = h.amount - order.amount } :: t;
+                };
+            }
+            |> deposit h.user "brb" order.amount
+            |> withdraw order.user "brb" order.amount
+            |> deposit order.user "usd" (order.amount *> h.rate)
+            |> withdraw h.user "usd" (order.amount *> h.rate)
+          in
+          let updated_order = { order with amount = 0 } in
+          sell_order_filler updated_order smaller_head
+(* let rec sell_order name amt (rate : float) db = let charge_orderer = withdraw
+   name "brb" amt db in { charge_orderer with orders = { db.orders with
+   sell_orders = { user = name; amount = amt; rate } :: db.orders.sell_orders;
+   }; } |> sort_orders *)
 
 (**[to_json db] is a json in string form that represents db*)
 let to_json (db : t) : string = db |> to_yojson |> Yojson.Safe.to_string
