2eee7e19be992fb08899b4e8a53797d1ccd5c05d
Anthony Fine <alf223@cornell.edu>
2022-12-13 19:24:50 -0500

    Added specifications to Database

--- src/database.ml
+++ src/database.ml
@@ -34,17 +34,12 @@ let empty_book = { buy_orders = []; sell_orders = [] }
 
 let new_database (name : string) : t =
   { users = []; db_name = name; orders = empty_book }
-(*TODO: Make this better. Like it could use from_json on an empty db file we
-  keep updated*)
 
 let db_name db = db.db_name
 
-(*[new_user db name] is the database b with a new user with username name
-  added *)
 let new_user db nm =
   { db with users = { name = nm; usd = 0; brb = 0 } :: db.users }
 
-(*[users db] is a list of the usernames of all the users in db*)
 let users db = List.map (fun x -> x.name) db.users
 
 let deposit name curr amt db =
@@ -62,8 +57,6 @@ let deposit name curr amt db =
 
 let withdraw name curr amt db = deposit name curr (-amt) db
 
-(* Example: [user_balance db name curr] is tony's balance of curr (in brb
-   cents) *)
 let user_balance db name curr =
   let cap = String.uppercase_ascii curr in
   let rec find_user users =
@@ -197,10 +190,5 @@ and sell_order_filler order db =
           in
           let updated_order = { order with amount = 0 } in
           sell_order_filler updated_order smaller_head
-(* let rec sell_order name amt (rate : float) db = let charge_orderer = withdraw
-   name "brb" amt db in { charge_orderer with orders = { db.orders with
-   sell_orders = { user = name; amount = amt; rate } :: db.orders.sell_orders;
-   }; } |> sort_orders *)
 
-(**[to_json db] is a json in string form that represents db*)
 let to_json (db : t) : string = db |> to_yojson |> Yojson.Safe.to_string
